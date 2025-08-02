6758e6158608b88a66a3294c3855c00cfe998efe
Florian Best <git@freeiam.org>
2025-08-03 00:07:38 +0200

    ci(sphinx): read version dynamically

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,21 @@
+import datetime
 import pathlib
 import sys
+
+from setuptools_scm import get_version
 
 
 sys.path.insert(0, str((pathlib.Path.cwd() / '..').resolve()))
 
+START = 2025
+year = datetime.datetime.now(tz=datetime.UTC).date().year
+
 project = 'FreeIAM'
-copyright = '2025'
+copyright = str(year) if year == START else f'2025-{year}'
 author = 'Florian Best'
-release = '0.1.0'
+release = get_version(root='../', relative_to=__file__)
+version = '.'.join(release.split('.')[:2])
+
 
 extensions = [
     'sphinx.ext.autodoc',
