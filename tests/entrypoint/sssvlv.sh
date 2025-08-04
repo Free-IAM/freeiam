db18800ad81181c619ef430c453668b73f65ed60
Florian Best <git@freeiam.org>
2025-08-04 23:44:35 +0200

    ci: enable SSS+VLV overlay in openldap

---
+++ tests/entrypoint/sssvlv.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+# Script to enable sssvlv overlay in OpenLDAP
+set -e
+
+cat > /tmp/sssvlv-overlay.ldif << 'EOF'
+dn: cn=module{1},cn=config
+objectClass: olcModuleList
+cn: module{1}
+olcModuleLoad: sssvlv
+
+dn: olcOverlay=sssvlv,olcDatabase={2}mdb,cn=config
+objectClass: olcOverlayConfig
+objectClass: olcSssVlvConfig
+olcOverlay: sssvlv
+olcSssVlvMax: 100
+olcSssVlvMaxKeys: 20
+EOF
+
+echo "Enabling sssvlv overlay in OpenLDAP configuration..."
+if slapcat -F /opt/bitnami/openldap/etc/slapd.d -b cn=config | grep -q sssvlv
+then
+    echo "SSS VLV overlay is already configured."
+    exit 0
+else
+    slapadd -F /opt/bitnami/openldap/etc/slapd.d -b cn=config -l /tmp/sssvlv-overlay.ldif || {
+        echo "NOTICE: slapadd failed to load sssvlv overlay. Check the cn=module{N} with \"slapcat -F /opt/bitnami/openldap/etc/slapd.d -b cn=config |grep 'cn=module'\""
+        exit 1
+    }
+fi
+
+echo "SSS VLV overlay has been configured."
