679ec1b8c7d60c28edbfe26c916c663d3f84ada9
Florian Best <git@freeiam.org>
2025-08-05 22:20:50 +0200

    docs(ldap.controls): add example code for usage of all LDAP controls

--- docs/examples/controls.py
+++ docs/examples/controls.py
@@ -1,7 +1,26 @@
-from ldap.controls.readentry import PostReadControl
+from freeiam import errors
+from freeiam.ldap.constants import LDAPChangeType, Mod, Scope
 
-from freeiam import ldap
-from freeiam.ldap._wrapper import Controls  # noqa: PLC2701
+# control imports
+from freeiam.ldap.controls import (
+    Controls,
+    assertion,
+    authorization_identity,
+    dereference,
+    get_effective_rights,
+    manage_dsa_information_tree,
+    matched_values,
+    persistent_search,
+    post_read,
+    pre_read,
+    proxy_authorization,
+    relax_rules,
+    session_tracking,
+)
+from freeiam.ldap.dn import DN
+
+
+# end control imports
 
 
 base_dn = 'dc=freeiam,dc=org'
@@ -11,35 +30,201 @@ def decode(self, values, encoding='UTF-8'):
     return values[0].decode(encoding)
 
 
-async def ldap_create_user_examples():
-    async with ldap.Connection('ldap://localhost:389') as conn:
-        ...  # do bind()
+async def example_ldap_pre_and_post_read_controls(conn):
+    # use PostRead Control
+    post_read_control = post_read(
+        [
+            '*',  # every regular attribute
+            '+',  # every operational attribute (metadata)
+        ],
+        criticality=True,
+    )
+    controls = Controls([post_read_control])
 
-        post_read_control = PostReadControl(
-            True,
-            [
-                '*',  # every regular attribute
-                '+',  # every operational attribute (metadata)
-            ],
-        )
-        controls = Controls([post_read_control])
-        # receive the e.g. entryUUID directly after creations
-        dn = f'uid=max.mustermann,{base_dn}'
-        attrs = {
-            'objectClass': [b'inetOrgPerson'],
-            'uid': [b'max.mustermann'],
-            'cn': [b'Max Mustermann'],
-            'givenName': [b'Max'],
-            'sn': [b'Mustermann'],
-            'mail': [b'max.mustermann@freeiam.org'],
-        }
-        result = await conn.add(dn, attrs, controls=controls)
-        print(result.dn, result.attrs)
+    # receive e.g. the entryUUID directly after creations
+    dn = f'uid=max.mustermann,{base_dn}'
+    attrs = {
+        'objectClass': [b'inetOrgPerson'],
+        'uid': [b'max.mustermann'],
+        'cn': [b'Max Mustermann'],
+        'givenName': [b'Max'],
+        'sn': [b'Mustermann'],
+        'mail': [b'max.mustermann@freeiam.org'],
+    }
+    result = await conn.add(dn, attrs, controls=controls)
+    print(result.dn, result.attrs)
 
-        entry = result.controls.get(post_read_control).entry
-        print(decode(entry['entryUUID']))
-        print(decode(entry['entryCSN']))
-        print(decode(entry['creatorsName']))
-        print(decode(entry['createTimestamp']))
-        print(decode(entry['modifyTimestamp']))
-        print(decode(entry['modifiersName']))
+    entry = result.controls.get(post_read_control).entry
+    print(decode(entry['entryUUID']))
+    print(decode(entry['entryCSN']))
+    print(decode(entry['creatorsName']))
+    print(decode(entry['createTimestamp']))
+    print(decode(entry['modifyTimestamp']))
+    print(decode(entry['modifiersName']))
+    # end use PostRead Control
+
+
+async def example_ldap_pre_read_control(conn, base_dn):
+    # use PreRead Control
+    pre_read_control = pre_read(
+        [
+            '*',  # every regular attribute
+            '+',  # every operational attribute (metadata)
+        ],
+        criticality=True,
+    )
+    controls = Controls([pre_read_control])
+    dn = f'uid=max.mustermann,{base_dn}'
+    result = await conn.modify(dn, ..., controls=controls)
+    entry = result.controls.get(pre_read_control).entry
+    # the attributes prior modification were:
+    print(decode(entry['sn']))
+    # end use PreRead Control
+
+
+def example_set_connection_server_controls(conn, base_dn):
+    # use connection wide control
+    # set controls for every operation on the connection
+    dn = f'cn=user02,ou=users,{base_dn}'
+    ctrl = pre_read(['givenName'], criticality=True)
+    conn.set_controls(Controls([ctrl]))
+
+    result = conn.modify_ml(dn, [(Mod.Replace, 'givenName', b'Foo')])
+    preread = result.controls.get(ctrl)
+    assert not preread.entry.get('givenName')
+
+    result = conn.modify_ml(dn, [(Mod.Replace, 'givenName', b'Test')])
+    preread = result.controls.get(ctrl)
+    assert preread.entry['givenName'][0] == b'Foo'
+
+    # unset via empty list
+    conn.set_controls(Controls([]))
+    # end use connection wide control
+
+
+def example_error_handling(conn, base_dn):
+    # start error handling
+    try:
+        ...  # some operation
+    except errors.UnavailableCriticalExtension:
+        ...  # make sure you handle unavailable extensions
+        ...  # when marked with criticality=True
+    # end error handling
+
+
+def example_assertion_control(conn, base_dn):
+    # use Assertion Control
+    dn = f'cn=user01,ou=users,{base_dn}'
+    ctrl = assertion('(sn=Bar1)', criticality=True)
+    conn.modify_ml(dn, [(Mod.Replace, 'sn', b'Foo1')], controls=Controls([ctrl]))
+
+    ctrl = assertion('(sn=Bar1)', criticality=True)
+    try:
+        conn.modify_ml(dn, [(Mod.Replace, 'sn', b'Foo2')], controls=Controls([ctrl]))
+    except errors.AssertionFailed:
+        ...  # this assertion is not True
+        ...  # do something
+    # end use Assertion Control
+
+
+def example_get_effective_rights(conn, base_dn):
+    # use GetEffectiveRights Control
+    ctrl = get_effective_rights(conn.whoami(), criticality=True)
+    result = conn.get(f'cn=user02,ou=users,{base_dn}', controls=Controls([ctrl]))
+    print(result.attr['entryLevelRights'])
+    print(result.attr['attributeLevelRights'])
+    # end use GetEffectiveRights Control
+
+
+def example_authorization_identity(conn, base_dn):
+    # use Authorization Identity Control
+    ctrl = authorization_identity(criticality=True)
+    result = conn.bind(
+        f'uid=testuser,ou=users,{base_dn}', 'secret', controls=Controls([ctrl])
+    )
+    res = result.controls.get(authorization_identity.response)
+    dn = res.authzId.decode('UTF-8')
+    if dn.startswith('dn:'):
+        DN(dn.removeprefix('dn:'))
+    # end use Authorization Identity Control
+
+
+def example_dereference(conn, base_dn):
+    # use Dereference Control (not supported by OpenLDAP)
+    ctrl = dereference({'member': 'cn'}, criticality=True)
+    conn.search(
+        f'ou=groups,{base_dn}',
+        Scope.Subtree,
+        '(objectClass=groupOfNames)',
+        controls=Controls([ctrl]),
+    )
+    # end use Dereference Control (not supported by OpenLDAP)
+
+
+def example_matched_values(conn, base_dn):
+    # use MatchedValues Control
+    ctrl = matched_values('(sn=Muster*)', criticality=True)
+    results = conn.search(
+        f'ou=users,{base_dn}',
+        Scope.Subtree,
+        '(objectClass=inetOrgPerson)',
+        controls=Controls([ctrl]),
+    )
+    print([obj.attr['sn'] for obj in results])  # contains only 'sn' attributes
+    # end use MatchedValues Control
+
+
+def example_persistent_search(conn, base_dn):
+    # use PersistentSearch Control (not supported by OpenLDAP)
+    ctrl = persistent_search(
+        [LDAPChangeType.Modify],
+        changes_only=True,
+        return_entry_change_control=True,
+        criticality=True,
+    )
+    # this blocks until any of the entry is modified!
+    conn.search(
+        f'ou=users,{base_dn}',
+        Scope.Subtree,
+        '(objectClass=inetOrgPerson)',
+        controls=Controls([ctrl]),
+    )
+    # end use PersistentSearch Control (not supported by OpenLDAP)
+
+
+def example_session_tracking(conn, base_dn):
+    # use SessionTrackingControl (not supported by OpenLDAP)
+    ctrl = session_tracking(
+        '127.0.0.1', 'test-client', '1.3.6.1.4.1.1466.115.121.1.15', 'Max.Mustermann'
+    )
+    conn.search(base_dn, Scope.Base, '(objectClass=*)', controls=Controls([ctrl]))
+    # end use SessionTrackingControl (not supported by OpenLDAP)
+
+
+def example_manage_dsa_information_tree(conn, base_dn):
+    # use ManageDSAITControl
+    ctrl = manage_dsa_information_tree(criticality=True)
+    conn.search(f'{base_dn}', Scope.Base, '(objectClass=*)', controls=Controls([ctrl]))
+    # end use ManageDSAITControl
+
+
+def example_relax_rules(conn, base_dn):
+    # use RelaxRules Control
+    ctrl = relax_rules(criticality=True)
+    custom_entry_uuid = b'00000000-0000-0000-0000-000000000000'
+    conn.modify_ml(
+        f'cn=user01,ou=users,{base_dn}',
+        [(Mod.Replace, 'entryUUID', custom_entry_uuid)],
+        controls=Controls([ctrl]),
+    )
+    assert conn.get_attr(f'cn=user01,ou=users,{base_dn}', 'entryUUID') == [
+        custom_entry_uuid
+    ]
+    # end use RelaxRules Control
+
+
+def example_proxy_authorization(conn, base_dn):
+    # use ProxyAuthz Control
+    ctrl = proxy_authorization(DN(f'cn=admin,{base_dn}'), criticality=True)
+    conn.search(f'{base_dn}', Scope.Base, '(objectClass=*)', controls=Controls([ctrl]))
+    # end use ProxyAuthz Control
--- docs/ldap_examples.rst
+++ docs/ldap_examples.rst
@@ -1,3 +1,4 @@
+
 Connection initialization (TLS, STARTTLS)
 =========================================
 
@@ -35,6 +36,8 @@ You can do everything also synchronously using the same API.
 
 Search and Pagination
 =====================
+
+.. _search-examples:
 
 .. literalinclude:: examples/search.py
    :language: python
@@ -149,10 +152,156 @@ FreeIAM provides the following mechanisms to safely and reliably work with LDAP 
 Using LDAP Controls
 ===================
 
+LDAP operations can be extended using LDAP controls, which provide additional features and behaviors.
+
+Controls can be passed explicitly with each operation, or applied globally to all operations via the connection object, as shown below:
+
 .. literalinclude:: examples/controls.py
    :language: python
-   :linenos:
-   :caption: LDAP Controls, Post-Read, Pre-Read, VLV, SSS, ...
+   :start-after: use connection wide control
+   :end-before: end use connection wide control
+
+The following example shows how to import the available controls:
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :caption: Importing LDAP server controls
+   :start-after: control imports
+   :end-before: end control imports
+
+Simple Paged Results, Virtual List View, and Server-Side Sorting Controls
+-------------------------------------------------------------------------
+
+The ``SimplePagedResultsControl``, ``VirtualListViewControl``, and ``ServerSideSortingControl`` are integrated into the ``search()``, ``search_paged()``, and ``search_paginated()`` methods.
+
+Refer to the :ref:`search-examples` for practical usage.
+
+PreRead and PostRead Controls
+-----------------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: PostRead Control
+   :start-after: use PostRead Control
+   :end-before: end use PostRead Control
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: PreRead Control
+   :start-after: use PreRead Control
+   :end-before: end use PreRead Control
+
+Assertion Control
+-----------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: Assertion Control
+   :start-after: use Assertion Control
+   :end-before: end use Assertion Control
+
+
+RelaxRules Control
+------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: RelaxRules Control
+   :start-after: use RelaxRules Control
+   :end-before: end use RelaxRules Control
+
+
+Proxy Authorization Control
+----------------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: ProxyAuthz Control
+   :start-after: use ProxyAuthz Control
+   :end-before: end use ProxyAuthz Control
+
+
+MatchedValues Control
+---------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: MatchedValues Control
+   :start-after: use MatchedValues Control
+   :end-before: end use MatchedValues Control
+
+
+PersistentSearch Control
+------------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: PersistentSearch Control (not supported by OpenLDAP)
+   :start-after: use PersistentSearch Control
+   :end-before: end use PersistentSearch Control
+
+
+SessionTracking Control
+-----------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: SessionTracking Control (not supported by OpenLDAP)
+   :start-after: use SessionTrackingControl
+   :end-before: end use SessionTrackingControl
+
+
+GetEffectiveRights Control
+--------------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: GetEffectiveRights Control
+   :start-after: use GetEffectiveRights Control
+   :end-before: end use GetEffectiveRights Control
+
+
+Authorization Identity Control
+------------------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: Authorization Identity Control
+   :start-after: use Authorization Identity Control
+   :end-before: end use Authorization Identity Control
+
+
+Dereference Control
+-------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: Dereference Control (not supported by OpenLDAP)
+   :start-after: use Dereference Control
+   :end-before: end use Dereference Control
+
+
+Manage DSA Information Tree Control
+-------------------
+
+.. literalinclude:: examples/controls.py
+   :language: python
+   :dedent: 4
+   :caption: ManageDSAIT Control
+   :start-after: use ManageDSAITControl
+   :end-before: end use ManageDSAITControl
+
 
 Extended Stuff
 ==============
