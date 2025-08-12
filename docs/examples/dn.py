518f822f02c8f9e523807e6374f5c6dca16d5c14
Florian Best <ldap@freeiam.org>
2025-08-12 20:30:45 +0200

    docs(ldap.dn): split examples into sections

--- docs/examples/dn.py
+++ docs/examples/dn.py
@@ -1,6 +1,6 @@
+# start COMPARE EXISTS
 import freeiam.errors
 import freeiam.ldap
-from freeiam.ldap.constants import AVA
 from freeiam.ldap.dn import DN
 
 
@@ -21,15 +21,23 @@ except freeiam.errors.NoSuchObject:
     ...  # the existing_object does not exists
 except freeiam.errors.InsufficientAccess:
     ...  # no "compare" rights exists for any of the RDNs in the tree
+# end COMPARE EXISTS
 
 
 # Second, the powerfull DN class provides utilities for every use case:
+# start DN CLASS
+# Creating and normalizing a DN
+from freeiam.ldap.constants import AVA
+from freeiam.ldap.dn import DN
+
+
 user_dn = 'uid = Max.Mustermann , cn = users , dc = freeiam, dc = org'
 dn = DN(user_dn)
 
 str(DN(user_dn))  # gives normalized: "uid=Max.Mustermann,cn=users,dc=freeiam,dc=org"
-repr(dn)  # <DN uid=Max.Mustermann,cn=users,dc=freeiam,dc=org>
+repr(dn)  # DN('uid=Max.Mustermann,cn=users,dc=freeiam,dc=org')
 
+# Handling invalid user input
 # make sure to handle errors for user given DNs:
 some_user_input_dn: str = ...
 try:
@@ -37,13 +45,14 @@ try:
 except freeiam.errors.InvalidDN as exc:
     print(exc)  # gives: Malformed DN syntax: "foo"
 
+# Hierarchy and navigation
 DN(user_dn).parent  # gives: DN("cn=users,dc=freeiam,dc=org")
 DN('dc=org').parent  # gives: None
 DN('dc=freeiam,dc=org').get_parent('dc=freeiam,dc=org')  # gives: None
 DN(user_dn).endswith('dc=freeiam,dc=org')  # True
 DN(user_dn).startswith('uid=Max.Mustermann,cn=users')  # True
 
-# constructing new DNs:
+# Constructing new DNs:
 base = DN('dc=freeiam,dc=org')
 user_input: str = ...
 DN.escape('#foo <+  ,=>"; bar#') == r'\#foo \<\+  \,\=\>\"\; bar#'
@@ -52,29 +61,35 @@ f'cn={DN.escape(user_input)},dc=freeiam,dc=org'
 
 str(DN.compose(('cn', 'admin'), 'ou=foo,ou=bar', base))
 # gives: 'cn=admin,ou=foo,ou=bar,dc=freeiam,dc=org'
-
 str(DN('cn=foo,cn=bar') + 'dc=freeiam,dc=org')  # DN('cn=foo,cn=bar,dc=freeiam,dc=org')
 
+# Working with DN sequences
 len(dn)  # 4
-dn[2]  # <DN dc=freeiam>
-dn[:-2]  # <DN uid=Max.Mustermann,cn=users>
+dn[2]  # DN('dc=freeiam')
+dn[:-2]  # DN('uid=Max.Mustermann,cn=users')
 
 
+# Accessing attributes and values
 dn.rdn  # ('uid', 'Max.Mustermann')
 dn.attribute  # 'uid'
 dn.value  # 'Max.Mustermann'
-multi_valued_rdn = DN('uid=1+cn=2,dc=3')
-multi_valued_rdn.multi_rdn  # (('uid', '1'), ('cn', '2'))
-multi_valued_rdn.attributes  # ('uid', 'cn')
-multi_valued_rdn.values  # ('1', '2')
 
+# Multi-Valued RDNs
+multi_valued_rdn = DN('uid=foo+cn=bar,dc=freeiam,dc=org')
+multi_valued_rdn.multi_rdn  # (('uid', 'foo'), ('cn', 'bar'))
+multi_valued_rdn.attributes  # ('uid', 'cn')
+multi_valued_rdn.values  # ('foo', 'bar')
+
+# Checking membership
 'cn=users' in dn  # True
 DN('cn=users') in dn  # True
 'cn=users,dc=freeiam' in dn  # False
 
+# Handling broken DNs
 # even broken DNs from Samba are supported:
 str(DN(r'uid=foo\?bar,cn=users')) == 'uid=foo?bar,cn=users'
 
+# Inspecting RDNs
 user_dn.rdns == [
     [('uid', 'Max.Mustermann', AVA.String)],
     [('cn', 'users', AVA.String)],
@@ -82,16 +97,19 @@ user_dn.rdns == [
     [('dc', 'org', AVA.String)],
 ]
 
+# Traversing a DN
 list(dn.walk('dc=freeiam,dc=org'))  # walks from the right-most:
 # [
-#                                <DN dc=freeiam,dc=org>,
-#                       <DN cn=users,dc=freeiam,dc=org>,
-#    <DN uid=Max.Mustermann,cn=users,dc=freeiam,dc=org>
+#                                DN('dc=freeiam,dc=org'),
+#                       DN('cn=users,dc=freeiam,dc=org'),
+#    DN('uid=Max.Mustermann,cn=users,dc=freeiam,dc=org')
 # ]
 
-# DN objects are hashable and suitable for use a dictionary keys
+# Using DN as Dictionary Keys
+# DN objects are hashable and suitable for use as dictionary keys
 {DN('UID = Administrator'): 1}[DN('uid=Administrator')] == 1
 
+# Utility methods
 DN.normalize('uid = Max.Mustermann , cn = users , dc = freeiam, dc = org')
 # "uid=Max.Mustermann,cn=users,dc=freeiam,dc=org"
 
@@ -99,20 +117,22 @@ isinstance(DN.get('uid=foo'), DN)
 isinstance(DN.get(DN('uid=foo')), DN)
 
 
-# comparisions, see the notes from the docs!
-DN('foo=1') == DN('foo=1')
-DN('foo=1') != DN('foo=2')
-DN('Foo=1') == DN('foo=1')
-DN('Foo=1') != DN('foo=2')
-DN('uid=Administrator') == DN('uid=administrator')
-DN('foo=Foo') != DN('foo=foo')
-DN('foo=1,bar=2') == DN('foo=1,bar=2')
-DN('bar=2,foo=1') != DN('foo=1,bar=2')
-DN('foo=1+bar=2') == DN('foo=1+bar=2')
-DN('bar=2+foo=1') == DN('foo=1+bar=2')
-DN('bar=2+Foo=1') == DN('foo=1+Bar=2')
-DN(f'foo={chr(92)}31') == DN(r'foo=1')
+# Comparisions
+# Comparisions can be done, please respect the notes above about the limits of comparing
+DN('cn=foo,dc=freeiam,dc=org') == DN('cn=foo,dc=freeiam,dc=org')
+DN('cn=foo,dc=freeiam,dc=org') != DN('cn=bar,dc=freeiam,dc=org')
+DN('CN=foo,dc=freeiam,dc=org') == DN('cn=foo,dc=freeiam,dc=org')
+DN('CN=foo,dc=freeiam,dc=org') != DN('cn=bar,dc=freeiam,dc=org')
+DN('uid=Administrator,dc=freeiam,dc=org') == DN('uid=administrator,dc=freeiam,dc=org')
+DN('cn=CN,dc=freeiam,dc=org') != DN('cn=cn,dc=freeiam,dc=org')
+DN('cn=foo,uid=bar,dc=freeiam,dc=org') == DN('cn=foo,uid=bar,dc=freeiam,dc=org')
+DN('uid=bar,cn=foo,dc=freeiam,dc=org') != DN('cn=foo,uid=bar,dc=freeiam,dc=org')
+DN('cn=foo+uid=bar,dc=freeiam,dc=org') == DN('cn=foo+uid=bar,dc=freeiam,dc=org')
+DN('uid=bar+cn=foo,dc=freeiam,dc=org') == DN('cn=foo+uid=bar,dc=freeiam,dc=org')
+DN('uid=bar+CN=foo,dc=freeiam,dc=org') == DN('cn=foo+UID=bar,dc=freeiam,dc=org')
+DN(r'cn=\66o\6f,dc=freeiam,dc=org') == DN(r'cn=foo,dc=freeiam,dc=org')
 
+# Removing duplicates
 DN.get_unique(
     [
         'CN=users,dc=freeiam,dc=org',
@@ -120,8 +140,9 @@ DN.get_unique(
         'cn = users,dc=freeiam,dc=org',
         'CN=Users,dc=freeiam,dc=org',
     ]
-)  # gives a set of unique DNs: {<DN CN=users,dc=freeiam,dc=org>}
+)  # gives a set of unique DNs: {DN('CN=users,dc=freeiam,dc=org')}
 
 DN.get_unique_str(
     DN.get_unique(['cn=foo', 'cn=bar']) - DN.get_unique(['cn = foo'])
-    ) == {'cn=bar'}  # fmt: skip
+) == {'cn=bar'}  # fmt: skip
+# end DN CLASS
--- docs/examples/ldap_dn.rst
+++ docs/examples/ldap_dn.rst
@@ -89,6 +89,234 @@ Always let the LDAP server handle DN logic and comparisons.
 
 FreeIAM provides the following mechanisms to safely and reliably work with LDAP DNs:
 
+Comparing DNs of existing objects
+---------------------------------
+
 .. literalinclude:: dn.py
    :language: python
-   :caption: LDAP DN handling
+   :caption: Comparing DNs of objects
+   :start-after: start COMPARE EXISTS
+   :end-before: end COMPARE EXISTS
+
+Object oriented DN class
+------------------------
+
+The :class:`~freeiam.ldap.dn.DN` class provides utilities for parsing,
+validating, normalizing, and manipulating LDAP Distinguished Names.
+
+Creating and normalizing a DN
+-----------------------------
+
+.. code-block:: pycon
+
+   >>> from freeiam.ldap.dn import DN
+   >>> user_dn = 'uid = Max.Mustermann , cn = users , dc = freeiam, dc = org'
+   >>> dn = DN(user_dn)
+   >>> str(dn)
+   'uid=Max.Mustermann,cn=users,dc=freeiam,dc=org'
+   >>> repr(dn)
+   'DN('uid=Max.Mustermann,cn=users,dc=freeiam,dc=org')'
+
+
+Handling invalid user input
+---------------------------
+
+.. code-block:: pycon
+
+   >>> import freeiam.errors
+   >>> try:
+   ...     DN('foo')
+   ... except freeiam.errors.InvalidDN as exc:
+   ...     print(exc)
+   Malformed DN syntax: "foo"
+
+
+Hierarchy and navigation
+------------------------
+
+.. code-block:: pycon
+
+   >>> DN(user_dn).parent
+   DN('cn=users,dc=freeiam,dc=org')
+   >>> DN('dc=org').parent is None
+   True
+   >>> DN('dc=freeiam,dc=org').get_parent('dc=freeiam,dc=org') is None  # get until certain base
+   True
+   >>> DN(user_dn).endswith('dc=freeiam,dc=org')
+   True
+   >>> DN(user_dn).startswith('uid=Max.Mustermann')
+   True
+
+
+Constructing new DNs
+--------------------
+
+.. code-block:: pycon
+
+   >>> base = DN('dc=freeiam,dc=org')
+   >>> DN.escape('#foo <+  ,=>"; bar#')
+   '\\#foo \\<\\+  \\,\\=\\>\\"\\; bar#'
+   >>> f'cn={DN.escape("admin")},dc=freeiam,dc=org'
+   'cn=admin,dc=freeiam,dc=org'
+   >>> str(DN.compose(('cn', 'admin'), 'ou=foo', base))
+   'cn=admin,ou=foo,dc=freeiam,dc=org'
+   >>> str(DN('cn=foo,cn=bar') + 'dc=freeiam,dc=org')
+   'cn=foo,cn=bar,dc=freeiam,dc=org'
+
+
+Working with DN sequences
+-------------------------
+
+.. code-block:: pycon
+
+   >>> len(dn)
+   4
+   >>> dn[2]
+   DN('dc=freeiam')
+   >>> dn[:-2]
+   DN('uid=Max.Mustermann,cn=users')
+
+
+Accessing attributes and values
+--------------------------------
+
+.. code-block:: pycon
+
+   >>> dn.rdn
+   ('uid', 'Max.Mustermann')
+   >>> dn.attribute
+   'uid'
+   >>> dn.value
+   'Max.Mustermann'
+
+
+Multi-Valued RDNs
+-----------------
+
+.. code-block:: pycon
+
+   >>> multi = DN('uid=foo+cn=bar,dc=freeiam,dc=org')
+   >>> multi.multi_rdn
+   (('uid', 'foo'), ('cn', 'bar'))
+   >>> multi.attributes
+   ('uid', 'cn')
+   >>> multi.values
+   ('foo', 'bar')
+
+
+Checking membership
+-------------------
+
+.. code-block:: pycon
+
+   >>> 'cn=users' in dn
+   True
+   >>> DN('cn=users') in dn
+   True
+   >>> 'cn=users,dc=freeiam' in dn
+   False
+
+
+Handling broken DNs
+-------------------
+
+Samba 4 returns broken DNs. This gets fixed.
+
+.. code-block:: pycon
+
+   >>> str(DN(r'uid=foo\?bar,cn=users'))
+   'uid=foo?bar,cn=users'
+
+
+Inspecting RDNs
+---------------
+
+.. code-block:: pycon
+
+   >>> from freeiam.ldap.constants import AVA
+   >>> dn.rdns == [
+   ...     [('uid', 'Max.Mustermann', AVA.String)],
+   ...     [('cn', 'users', AVA.String)],
+   ...     [('dc', 'freeiam', AVA.String)],
+   ...     [('dc', 'org', AVA.String)],
+   ... ]
+   True
+
+
+Traversing a DN
+---------------
+
+.. code-block:: pycon
+
+   >>> list(dn.walk('dc=freeiam,dc=org'))
+   [DN('dc=freeiam,dc=org'),
+    DN('cn=users,dc=freeiam,dc=org'),
+    DN('uid=Max.Mustermann,cn=users,dc=freeiam,dc=org')]
+
+
+Using DN as Dictionary Keys
+---------------------------
+
+DN objects are hashable and suitable for use as dictionary keys.
+
+.. code-block:: pycon
+
+   >>> {DN('UID = Administrator'): 1}[DN('uid=Administrator')]
+   1
+
+
+Utility methods
+---------------
+
+.. code-block:: pycon
+
+   >>> DN.normalize(user_dn)
+   'uid=Max.Mustermann,cn=users,dc=freeiam,dc=org'
+   >>> isinstance(DN.get('uid=foo'), DN)
+   True
+   >>> isinstance(DN.get(DN('uid=foo')), DN)
+   True
+
+
+Comparisons
+-----------
+
+Comparisions can be done, please respect the notes above about the limits of comparing.
+
+.. code-block:: pycon
+
+   >>> DN('cn=foo,dc=freeiam,dc=org') == DN('cn=foo,dc=freeiam,dc=org')
+   True
+   >>> DN('cn=foo,dc=freeiam,dc=org') != DN('cn=bar,dc=freeiam,dc=org')
+   True
+   >>> DN('CN=foo,dc=freeiam,dc=org') == DN('cn=foo,dc=freeiam,dc=org')
+   True
+   >>> DN('uid=Administrator,dc=freeiam,dc=org') == DN('uid=administrator,dc=freeiam,dc=org')
+   True
+   >>> DN('cn=CN,dc=freeiam,dc=org') != DN('cn=cn,dc=freeiam,dc=org')
+   True
+   >>> DN('cn=foo+uid=bar,dc=freeiam,dc=org') == DN('cn=foo+uid=bar,dc=freeiam,dc=org')
+   True
+   >>> DN('uid=bar+cn=foo,dc=freeiam,dc=org') == DN('cn=foo+uid=bar,dc=freeiam,dc=org')
+   True
+   >>> DN(r'cn=\66o\6f,dc=freeiam,dc=org') == DN(r'cn=foo,dc=freeiam,dc=org')
+   True
+
+
+Removing duplicates
+-------------------
+
+.. code-block:: pycon
+
+   >>> DN.get_unique([
+   ...     'CN=users,dc=freeiam,dc=org',
+   ...     'cn=users,dc=freeiam,dc=org',
+   ...     'cn = users,dc=freeiam,dc=org',
+   ...     'CN=Users,dc=freeiam,dc=org',
+   ... ])
+   {DN('CN=users,dc=freeiam,dc=org')}
+
+   >>> DN.get_unique_str(
+   ...     DN.get_unique(['cn=foo', 'cn=bar']) - DN.get_unique(['cn = foo'])
+   ... ) == {'cn=bar'}
+   True
